34f052d50a0c6a1397df8cdf33d73c9416de4dea
Alex Wu <alexcellence@gmail.com>
2020-07-01 14:52:10 -0700

    Pass tests for functional pattern - stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,27 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
+
+  var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    counter++;
+    someInstance.storage[counter] = value;
   };
 
   someInstance.pop = function() {
+    var popped = someInstance.storage[counter];
+    delete someInstance.storage[counter];
+    if (counter > 0) {
+      counter--;
+    }
+    return popped;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
