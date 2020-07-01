aed2eb7926373ea26cf5014223300bc15d413598
Alex Wu <alexcellence@gmail.com>
2020-07-01 16:10:58 -0700

    Pass all tests for the functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,38 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var counter = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add a string to the back of the queue
+    counter++;
+    storage[counter] = value;
+    console.log('enqueue ' , storage, Object.keys(storage));
   };
 
   someInstance.dequeue = function() {
+    // remove and return the string at the front of the queue
+
+    var keys = Object.keys(storage);
+    var shiftPosition = keys.shift();
+    // create variable for the item that is being removed
+    var dequeued = storage[shiftPosition];
+
+    // delete that first item
+    delete storage[shiftPosition];
+    // change counter
+    // if (counter > 0) {
+    //   counter--;
+    // }
+
+    // return item that just got deleted
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
