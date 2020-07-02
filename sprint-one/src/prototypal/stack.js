ea8fd5210545389dc955f408d129eca6bbdff93e
Alex Wu <alexcellence@gmail.com>
2020-07-01 22:14:31 -0700

    Pass tests for stack in the prototypal instantiation

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var obj = Object.create(stackMethods);
+
+  obj.storage = {};
+  obj.counter = 0;
+
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.counter++;
+    this.storage[value] = value;
+  },
+
+  pop: function() {
+    var keys = Object.keys(this.storage);
+    var lastKey = keys[keys.length - 1];
+    var popped = this.storage[lastKey];
+    if (this.counter > 0) {
+      this.counter--;
+    }
+    delete this.storage[lastKey];
+    return popped;
+  },
+
+  size: function() {
+    return this.counter;
+  },
+};
 
 
