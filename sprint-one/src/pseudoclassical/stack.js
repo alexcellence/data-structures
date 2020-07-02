67b0776213b18d4fac9552d0b101a77c42b20c78
Alex Wu <alexcellence@gmail.com>
2020-07-01 22:46:00 -0700

    Pass tests for stack in the pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
-var Stack = function() {
+var Stack = function(storage, counter) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+
+  this.counter = 0;
+
 };
 
+Stack.prototype.push = function(value) {
+  this.counter++;
+  this.storage[value] = value;
+};
 
+Stack.prototype.pop = function() {
+  var keys = Object.keys(this.storage);
+  var lastKey = keys[keys.length - 1];
+  var popped = this.storage[lastKey];
+  if (this.counter > 0) {
+    this.counter--;
+  }
+  delete this.storage[lastKey];
+  return popped;
+};
+
+Stack.prototype.size = function () {
+  return this.counter;
+};
