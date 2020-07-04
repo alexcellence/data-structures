a247744daf85b9d694d2da504269889213ef9141
Alex Wu <alexcellence@gmail.com>
2020-07-03 22:42:32 -0700

    Pass tests for binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,61 @@
-var BinarySearchTree = function(value) {
-};
+// left property, right property
+// insert method, contains method, depthFirstLog method
+// left of parent node is less than parents value, right is more than parents value
 
+var BinarySearchTree = function(value) {
+  var tree = {};
+
+  tree.value = value;
+  tree.left = undefined;
+  tree.right = undefined;
+
+  tree.insert = function (value) {
+    let node = new BinarySearchTree(value);
+
+    let recurse = function (element) {
+      if (element.value < value && !element.right) {
+        element.right = node;
+      } else if (element.value > value && !element.left) {
+        element.left = node;
+      } else if (element.value > value) {
+        recurse(element.left);
+      } else if (element.value < value) {
+        recurse(element.right);
+      }
+    };
+    recurse(this);
+  };
+
+  tree.contains = function (value) {
+
+    if (this.value === value) { // 4
+      return true;
+    } else if (this.value < value && this.right) {
+      return this.right.contains(value);
+    } else if (this.value > value && this.left) {
+      return this.left.contains(value);
+    } else {
+      return false;
+    }
+
+  };
+
+  tree.depthFirstLog = function (callback) {
+
+    callback(this.value);
+
+    if (this.left) {
+      this.left.depthFirstLog(callback);
+    }
+
+    if (this.right) {
+      this.right.depthFirstLog(callback);
+    }
+
+  };
+
+  return tree;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
